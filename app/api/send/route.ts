95d06d4bdc6be7177e29b794f045f52529ffd48f
M40 <modev.404@gmail.com>
2024-07-18 17:13:54 +0300

    Send Batch Emails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,18 +6,48 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req:NextRequest) {
     try {
-        const { error } = await resend.emails.send({
-            from:"delivered@resend.dev",
-            to:"modev.404@gmail.com",
-            subject:"Notification",
-            react:EmailTemplate({username:"Mukhtaar"})
-        })
+                
+        // const { error,data } = await resend.emails.send(
+        //     {
+        //         from:"Acme <delivered@resend.dev>",
+        //         to:"modev.404@gmail.com",
+        //         subject:"Notification",
+        //         react:EmailTemplate({username:"Mukhtaar"}),
+        //         reply_to:'info@support.com'
+        //     }
+        // )
+
+        // Send Batch Emails
+        // Trigger up to 100 batch emails at once. 
+        const { error,data } = await resend.batch.send([
+            {
+                from:"Acme <delivered@resend.dev>",
+                to:"modev.404@gmail.com",
+                subject:"Notification",
+                react:EmailTemplate({username:"Mukhtaar"}),
+                reply_to:'info@support.com'
+            },
+            {
+                from:"Acme <delivered@resend.dev>",
+                to:"modev.404@gmail.com",
+                subject:"Notification",
+                react:EmailTemplate({username:"Faarax"}),
+                reply_to:'info@support.com'
+            },
+            {
+                from:"Acme <delivered@resend.dev>",
+                to:"modev.404@gmail.com",
+                subject:"Notification",
+                react:EmailTemplate({username:"Geele"}),
+                reply_to:'info@support.com'
+            }
+        ])
 
         if(error){
             return NextResponse.json({message:"Unexpected error",error},{status:500})
         }
 
-        return NextResponse.json("ok",{status:201})
+        return NextResponse.json(data,{status:201})
     } catch (error) {
         return NextResponse.json({message:"Unexpected error"},{status:500})
     }
